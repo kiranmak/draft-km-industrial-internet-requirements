b8f375a1b716d1cc4b98c0c381d85fd24ceb838e
PUCSD Group <kiranmak@gmail.com>
2021-04-24 16:57:57 -0700

    skeleton draft

--- .targets.mk
+++ .targets.mk
@@ -1,4 +1,2 @@
 TARGETS_DRAFTS := draft-ietf-flexadress-requirements
 TARGETS_TAGS := 
-draft-ietf-flexadress-requirements-00.md: draft-ietf-flexadress-requirements.md
-	sed -e 's/draft-ietf-flexadress-requirements-latest/draft-ietf-flexadress-requirements-00/g' $< >$@
