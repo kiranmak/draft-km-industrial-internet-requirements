c1d52945684203e7e25b213c69b8429e3514bbef
Kiran Makhijani <kiranmak@gmail.com>
2021-05-26 14:07:56 -0700

    Delete .targets.mk

--- .targets.mk
+++
@@ -1,4 +0,0 @@
-TARGETS_DRAFTS := draft-ietf-flexadress-requirements
-TARGETS_TAGS := 
-draft-ietf-flexadress-requirements-00.txt: draft-ietf-flexadress-requirements.txt
-	sed -e 's/draft-ietf-flexadress-requirements-latest/draft-ietf-flexadress-requirements-00/g' -e 's/draft-ietf-flexadress-requirements-latest/draft-ietf-flexadress-requirements-00/g' -e 's/draft-ietf-flexadress-requirements-latest/draft-ietf-flexadress-requirements-00/g' $< >$@
